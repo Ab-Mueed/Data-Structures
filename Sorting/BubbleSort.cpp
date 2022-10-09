a6fb7addd83a6272d1866170ea204bada2490e0d
Ab-Mueed <95278577+Ab-Mueed@users.noreply.github.com>
2022-10-09 19:17:39 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 void bubbleSort(int arr[], int n)
 {
-    int temp;
-    for(int i = 0; i < n; i++)
+    int temp,flag;
+    for(int i = 0; i < n-1; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        flag = 0;
+       
+        for (int j = 0; j < n - 1-i; j++)
         {
             if(arr[j] > arr[j+1])
             {
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = 1;
             }
-        }    
+        }
+        if(flag==0)
+            break;
     }
 }
 int main()
